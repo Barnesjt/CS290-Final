dad896a1129df8f8b24a6d2ea6fa19fc8a4b6c74
Jack Barnes <barnesjt05@gmail.com>
2018-12-05 01:45:04 -0800

    basic design implemented

---
+++ public/index.js
@@ -0,0 +1,14 @@
+function toggleUploadBox() {
+    var uploadBox = document.querySelector('.upload-dialog-box');
+    uploadBox.classList.toggle('hidden');
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    var uploadButton = document.querySelector('.new-upload-button');
+    uploadButton.addEventListener('click', toggleUploadBox);
+
+    var cancelButton = document.querySelector('.cancel-button');
+    cancelButton.addEventListener('click', toggleUploadBox);
+
+});
